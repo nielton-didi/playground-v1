9efb6f186ba083f8a2e02debdc8fc2755d0e3576
Nielton Didi <nieltondidi@gmail.com>
2024-12-15 16:50:43 +0800

    feat: added base layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<main class="min-h-screen w-screen overscroll-contain scroll-smooth overflow-x-hidden">
+	<section class="flex h-screen w-full flex-col items-center justify-center">
+		<p>Section 1</p>
+	</section>
+	<section class="flex h-screen w-full flex-col items-center justify-center">
+		<p>Section 2</p>
+	</section>
+	<section class="flex h-screen w-full flex-col items-center justify-center">
+		<p>Section 3</p>
+	</section>
+	<section class="flex h-screen w-full flex-col items-center justify-center">
+		<p>Section 4</p>
+	</section>
+	<section class="flex h-screen w-full flex-col items-center justify-center">
+		<p>Section 5</p>
+	</section>
+</main>
+
